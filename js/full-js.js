ae9ccc19b58e444530c31f4226b97beee9b7475c
Duplexificate <munavido@gmail.com>
2024-05-26 13:33:38 +0200

    vietnam?

--- js/full-js.js
+++ js/full-js.js
@@ -62,16 +62,19 @@ async function SurveyPopup (event) {
     setTimeout(() => {
         NoThanks();
         let no = new DocumentFragment();
-        let a = document.createElement("a");
+        let a = document.createElement("button");
         let p = document.createElement("p");
 
         a.append(p);
         p.innerHTML = "No Thanks.";
-
+        p.style.padding = "0px";
 
         a.style.position = "relative";
-        a.style.top = "-7.5em";
+        a.style.top = "-6.5em";
         a.style.padding = "0px 10px";
+        a.style.backgroundColor = "#ffffff00";
+        a.style.border = "0px solid red";
+        a.style.textDecoration = "underline";
         no.append(a);
 
         a.id = "no-t";
@@ -81,6 +84,16 @@ async function SurveyPopup (event) {
         document.getElementById("no-t").addEventListener("mousedown", () => {
             Survey.style.opacity = "0%";
             p.innerHTML = "Got it.";
+        });
+        document.getElementById("no-t").addEventListener("keypress", (event) => {
+
+            if (event.key === "Enter") {
+                // Cancel the default action, if needed
+                event.preventDefault();
+                // Trigger the button element with a click
+                Survey.style.opacity = "0%";
+                p.innerHTML = "Got it.";
+              }
         });
     }, 7500);
     
